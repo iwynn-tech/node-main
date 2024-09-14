7f7554417fb598813db8bad54f0659f79c261507
iwynntech <iwynntech@gmail.com>
2024-09-14 13:23:09 +0200

    change port

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = parseInt(process.env.PORT) || 8080;
+// const routes = require('./routes/v1');
+
+
+// parse json request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// v1 api routes
+// app.use('/v1', routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +19,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
