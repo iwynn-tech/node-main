49d7cae1c3ac6e353cb8ebd244c9507841cc11c9
iwynntech <iwynntech@gmail.com>
2024-09-14 20:07:14 +0200

    ganti nama controller

---
+++ controllers/softGamingController.js
@@ -0,0 +1,27 @@
+const { db } = require("../config/firebase");
+
+const crypto = require('crypto');
+const base = Object.assign({}, requestBody);
+delete base.hmac;
+if ('actions' in base) {
+let actions = '';
+for (const action of base.actions) {
+        Object.keys(action).sort().forEach(key => actions += action[key]);
+    }
+    base.actions = actions;
+}
+const hash = crypto.createHash('sha256');
+const hmac = crypto.createHmac('sha256', hash.update(secretKey).digest('buffer'));
+let hmacBase = '';
+Object.keys(base).sort().forEach(key => hmacBase += base[key]);
+const hmacString = hmac.update(hmacBase).digest('hex');
+
+
+
+const callback = async (req, res) => {
+    db.collection('callback').add({type:'softgaming',...req.body})
+    .then((e)=> res.status(200).json({ status: true,message:e.id }))
+    .catch((e)=>res.status(400).json({status:false,message:e.message}))
+  };
+
+  module.exports = { callback };
--- routes/softgaming.route.js
+++ routes/softgaming.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const softGamingController = require('../controllers/softGamingRouter')
+const softGamingController = require('../controllers/softGamingController')
 
 
 const router = express.Router();
