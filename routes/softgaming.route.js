efe56f24cf909815ac089943ec1818615f879fbb
iwynntech <iwynntech@gmail.com>
2024-09-19 11:28:15 +0200

    update

--- routes/softgaming.route.js
+++ routes/softgaming.route.js
@@ -4,6 +4,7 @@ const softGamingController = require('../controllers/softGamingController')
 
 const router = express.Router();
 
+// api/v1/softgaming/
 router.post('/get',softGamingController.get);
 router.post('/user',softGamingController.user);
 // router.post('/authentication',softGamingController.authentication);
